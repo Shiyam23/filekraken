73e40c16d9fb9340fea57196e914e59754b146ad
Shiyam23 <39777757+Shiyam23@users.noreply.github.com>
2023-05-10 23:54:25 +0200

    Add group insert implementation

--- lib/model/group_config.dart
+++ lib/model/group_config.dart
@@ -1,19 +1,29 @@
-class GroupConfig {
+import 'package:petitparser/petitparser.dart';
 
+class GroupConfig {
   GroupConfig({
     required this.groups
   });
-
   final List<GroupOption> groups;
 }
 
 class GroupOption {
-
   GroupOption({
     this.match,
     this.groupName
   });
-
   String? match;
   String? groupName;
+}
+
+Parser _match = anyOf(",").neg().plus().flatten().map((value) => value.trimLeft());
+Parser _separator = char(",").map((value) => "");
+Parser _expression = _match & (_separator & _match).map((value) => value[1]).star();
+
+List<String> parseGroupMatch(String groupMatch) {
+  Result result = _expression.parse(groupMatch);
+  if (result.isSuccess) {
+    return [result.value[0], ...result.value[1]];
+  }
+  throw ArgumentError("Invalid groupmatch identifier");
 }
--- lib/pages/insert_page.dart
+++ lib/pages/insert_page.dart
@@ -84,16 +84,55 @@ class _InsertPageState extends State<InsertPage> {
     if (_selectedFiles == null || _selectedFiles!.isEmpty) {
       return;
     }
-    for (int i = 0; i < _selectedFiles!.length; i++) {
-      String selectedFilePath = _selectedFiles![i];
-      String fileBasename = basenameWithoutExtension(selectedFilePath);
-      String directoryName = modifyName(fileBasename, i, pathModifierConfig, {"s":""});
-      String newDirectoryPath = join(dirname(selectedFilePath), directoryName);
-      Directory newDirectory = Directory(newDirectoryPath);
-      if (!await newDirectory.exists()) {
-        newDirectory.create();
-        File selectedFile = File(selectedFilePath);
-        selectedFile.rename(join(newDirectoryPath, basename(selectedFilePath)));
+
+    List<GroupOption> nonEmptyGroups = groupConfig.groups
+    .where((group) => group.match != null && group.match != "")
+    .where((group) => group.groupName != null && group.groupName != "")
+    .toList();
+
+    if (nonEmptyGroups.isNotEmpty) {
+      Map<String, List<String>> fileGroups = {
+      for (GroupOption group in nonEmptyGroups) group.groupName!:[] 
+      };
+      for (int i = 0; i < _selectedFiles!.length; i++) {
+        String selectedFilePath = _selectedFiles![i];
+        String fileBasename = basename(selectedFilePath);
+        if (nonEmptyGroups.isNotEmpty) {
+          for (GroupOption group in nonEmptyGroups) {
+            String groupMatch = group.match!;
+            String groupName = group.groupName!;
+            List<String> matches = parseGroupMatch(groupMatch);
+            for (String match in matches) {
+              if (fileBasename.contains(match)) {
+              fileGroups[groupName]!.add(selectedFilePath);
+              break;
+              }
+            }
+          }
+        } 
+      }
+      for (String groupName in fileGroups.keys) {
+        if (fileGroups[groupName]!.isEmpty) continue;
+        Directory groupDirectory = Directory(join(_rootPath!, groupName));
+        if (!await groupDirectory.exists()) groupDirectory.create();
+        for (String filePath in fileGroups[groupName]!) {
+          File file = File(filePath);
+          file.rename(join(groupDirectory.path, basename(filePath)));
+        }
+      }
+    }
+    else {
+      for (int i = 0; i < _selectedFiles!.length; i++) {
+        String selectedFilePath = _selectedFiles![i];
+        String fileBasename = basenameWithoutExtension(selectedFilePath);
+        String directoryName = modifyName(fileBasename, i, pathModifierConfig, {"s":""});
+        String newDirectoryPath = join(dirname(selectedFilePath), directoryName);
+        Directory newDirectory = Directory(newDirectoryPath);
+        if (!await newDirectory.exists()) {
+          newDirectory.create();
+          File selectedFile = File(selectedFilePath);
+          selectedFile.rename(join(newDirectoryPath, basename(selectedFilePath)));
+        }
       }
     }
   }
