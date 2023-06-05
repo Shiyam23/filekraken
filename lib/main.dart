b7daa2dad02f4305b52fdc6ed0ab74400d7b4e1a
Shiyam23 <39777757+Shiyam23@users.noreply.github.com>
2023-06-05 19:41:01 +0200

    Remove unused l10n package

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:filekraken/service/database.dart';
 import 'package:filekraken/theme/fk_dark_theme.dart';
 import 'package:filekraken/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'layout.dart';
@@ -43,15 +42,6 @@ class MyApp extends StatelessWidget {
               theme: initialTheme.themeData,
               restorationScopeId: "filekraken",
               debugShowCheckedModeBanner: false,
-              localizationsDelegates: [
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-              ],
-              supportedLocales: const [
-                Locale('en'), // English
-                Locale('de'), // German
-              ],
               home: const Scaffold(
                 body: Layout()
               )
