a8fa0bcd7eebc40280ed98bb51cf2701eb8936bd
Shiyam23 <39777757+Shiyam23@users.noreply.github.com>
2024-11-15 19:38:57 +0100

    Remove variable list and history buttons from titlebar

--- lib/components/titlebar.dart
+++ lib/components/titlebar.dart
@@ -1,56 +1,49 @@
 import 'dart:io';
 import 'package:filekraken/components/module_page.dart';
 import 'package:filekraken/components/navigation_rail.dart';
-import 'package:filekraken/components/titlebar/history_widget.dart';
-import 'package:filekraken/components/titlebar/variable_widget.dart';
 import 'package:filekraken/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class TitleBar extends StatelessWidget {
-  
   const TitleBar({super.key, required this.height});
 
   final double height;
   @override
   Widget build(BuildContext context) {
-
     FKTheme theme = FKThemeWidget.of(context)!.theme;
-
     return SizedBox(
       height: height,
       child: WindowTitleBarBox(
         child: ColoredBox(
-          color: const Color(0xFF18212B),
+          color: Colors.white,
           child: Stack(
             children: [
               MoveWindow(
                 child: Container(
                   padding: const EdgeInsets.only(left: 15.0),
-                  alignment: Platform.isMacOS ? Alignment.center : Alignment.centerLeft,
+                  alignment: Platform.isMacOS
+                      ? Alignment.center
+                      : Alignment.centerLeft,
                 ),
               ),
               Padding(
-                padding: Platform.isMacOS ? const EdgeInsets.only(left: 70) : EdgeInsets.zero,
+                padding: Platform.isMacOS
+                    ? const EdgeInsets.only(left: 70)
+                    : EdgeInsets.zero,
                 child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const TitleBarMenuButton(
-                      route: VariableListWidget(),
-                      title: Text("Variables"),
-                    ),
-                    const TitleBarMenuButton(
-                      route: HistoryWidget(),
-                      title: Text("History"),
-                    ),
-                    const Spacer(),
-                    MinimizeWindowButton(colors: theme.windowButtonColors),
-                    appWindow.isMaximized ? 
-                      RestoreWindowButton(colors: theme.windowButtonColors) : 
-                      MaximizeWindowButton(colors: theme.windowButtonColors),
-                    CloseWindowButton(colors: theme.closeButtonColors,)
-                  ]
-                ),
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      MinimizeWindowButton(colors: theme.windowButtonColors),
+                      appWindow.isMaximized
+                          ? RestoreWindowButton(
+                              colors: theme.windowButtonColors)
+                          : MaximizeWindowButton(
+                              colors: theme.windowButtonColors),
+                      CloseWindowButton(
+                        colors: theme.closeButtonColors,
+                      )
+                    ]),
               ),
             ],
           ),
@@ -61,11 +54,8 @@ class TitleBar extends StatelessWidget {
 }
 
 class TitleBarMenuButton extends ConsumerWidget {
-  const TitleBarMenuButton({
-    super.key,
-    required this.title,
-    required this.route
-  });
+  const TitleBarMenuButton(
+      {super.key, required this.title, required this.route});
 
   final Widget title;
   final Widget route;
@@ -76,27 +66,22 @@ class TitleBarMenuButton extends ConsumerWidget {
       padding: const EdgeInsets.all(10),
       alignment: Alignment.center,
       child: OutlinedButton(
-        style: ButtonStyle(
-          fixedSize: const WidgetStatePropertyAll(Size(120, 40)),
-          shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(5)
-          )),
-          foregroundColor: const WidgetStatePropertyAll(Colors.white),
-          backgroundColor: const WidgetStatePropertyAll(Colors.blueGrey)
-        ),
-        child: title,
-        onPressed: () {
-          final GlobalKey<NavigatorState> navigatorKey = ref.read(navigatorProvider);
-          navigatorKey.currentState?.pushReplacement(
-            PageRouteBuilder(
-              reverseTransitionDuration: Duration.zero,
-              transitionDuration: Duration.zero,
-              pageBuilder: (context, a, aa) => route
-            )
-          );
-          ref.read(navigationRailSelectedIndexProvider.notifier).state = null;
-        }
-      ),
+          style: ButtonStyle(
+              fixedSize: const WidgetStatePropertyAll(Size(120, 40)),
+              shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5))),
+              foregroundColor: const WidgetStatePropertyAll(Colors.white),
+              backgroundColor: const WidgetStatePropertyAll(Colors.blueGrey)),
+          child: title,
+          onPressed: () {
+            final GlobalKey<NavigatorState> navigatorKey =
+                ref.read(navigatorProvider);
+            navigatorKey.currentState?.pushReplacement(PageRouteBuilder(
+                reverseTransitionDuration: Duration.zero,
+                transitionDuration: Duration.zero,
+                pageBuilder: (context, a, aa) => route));
+            ref.read(navigationRailSelectedIndexProvider.notifier).state = null;
+          }),
     );
   }
 }
